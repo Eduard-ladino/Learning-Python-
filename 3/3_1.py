a7e4f2e2ce1e4ff05d337aa1394e4402025fee72
camilo-camargo <camilocamargo49@gmail.com>
2022-11-28 19:18:21 -0500

    Exercises 3_1

---
+++ 3/3_1.py
@@ -0,0 +1,6 @@
+#1.1 Create a string literal with your name and try edit it
+#1.2 Create a string literal with the all escape sequence
+#1.3 Create a bytes with some string object
+#1.4 Create a bytearray with your name
+#1.5 Create a bytearray and edit it with something
+
